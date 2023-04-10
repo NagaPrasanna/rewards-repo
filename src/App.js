e48707435ee387aebd22afdab7446cfd3bd6d11f
NagaPrasanna <nagaprasanna@kalitechnologies.com>
2023-04-09 22:26:45 -0500

    New changes ti rewards-repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, Fragment } from 'react';
 import './App.css';
+import data from './data';
 
 function App() {
+  const [loadData, setloadData] = useState({});
+  const [rewardCal, setRewardCal] = useState({});
+  const [monthlyTrans, setMonthlyTrans] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState("");
+
+
+  useEffect(() => {
+    console.log('Help');
+    setloadData({ ...data });
+    setUsers([...Object.keys(data)]);
+
+  }, []);
+
+
+  const userSelect = (value) => {
+    setSelectedUser(value);
+    let selectedUserData = loadData[value];
+
+    let monthTot = {
+      1: {
+        amountSpent: [],
+        rewards: 0,},
+      2: {
+        amountSpent: [],
+        rewards: 0,
+      },
+      3: {
+        amountSpent: [],
+        rewards: 0,
+      },
+    };
+    for (let i = 0; i < selectedUserData.length; i++) {
+      let month = new Date(selectedUserData[i]['date']);
+      if (month.getMonth() + 1 == 1 || month.getMonth() + 1 == 2 || month.getMonth() + 1 == 3) {
+        monthTot[month.getMonth() + 1]['amountSpent'].push(selectedUserData[i]['amount']);
+      }
+    }
+    for (let key in monthTot) {
+      let total_month_rewards = 0;
+      for (let i = 0; i < monthTot[key]['amountSpent'].length; i++) {
+        let price = monthTot[key]['amountSpent'][i];
+
+        total_month_rewards = total_month_rewards + reward(price);
+      }
+      monthTot[key]['rewards'] = total_month_rewards;
+    }
+    console.log(monthTot)
+    setRewardCal({ ...monthTot });
+    setMonthlyTrans([...selectedUserData]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+   
+    <div className='rewardDashboard'>
+      <h2 style={{ textAlign: "center" }}>User Rewards Dashborad</h2>
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <label>Select Customer</label>
+        <select onChange={e => userSelect(e.target.value)} value={selectedUser} >
+          <option value="" disabled>Select User</option>
+          {users.map((item, index) => {
+            return (
+              <option key={index} value={item}> {item} </option>
+            );
+          })}
+        </select>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      {Object.keys(rewardCal).length > 0 &&
+        <Fragment>
+          <table className="customers" >
+            <thead>
+              <tr>
+                <th>Month</th>
+                <th>Rewards</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>First Month</td>
+                <td>{rewardCal[1]["rewards"]}</td>
+              </tr>
+              <tr>
+                <td>Second Month</td>
+                <td>{rewardCal[2]["rewards"]}</td>
+              </tr>
+              <tr>
+                <td>Third Month</td>
+                <td>{rewardCal[3]["rewards"]}</td>
+              </tr>
+              <tr>
+                <td>Total Reward</td>
+                <td>{rewardCal[1]["rewards"] + rewardCal[2]["rewards"] + rewardCal[3]["rewards"]}</td>
+              </tr>
+            </tbody>
+          </table>
+          <h4>User Transactions</h4>
+          {monthlyTrans.length > 0 ?
+            <table className="customers">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Amount</th>
+                  <th>Rewards</th>
+                </tr>
+
+              </thead>
+              <tbody>
+                {monthlyTrans.map((item, index) => {
+                  return <tr key={index}>
+                    <td>{item["date"]}</td>
+                    <td>{item["amount"]}</td>
+                    <td>{reward(item["amount"])}</td>
+                  </tr>
+                })}
+              </tbody>
+            </table>
+            : <div>No Transactions Found</div>}
+          
+        </Fragment>
+      }
+
+
+    </ div >
   );
 }
 
 export default App;
+
+function reward(price) {
+  let rewardPoints = 0;
+  if (price > 100) {
+    rewardPoints = (price - 100) * 1;
+  }
+  if (price > 50) {
+    rewardPoints = rewardPoints + (price - 50);
+  }
+  return rewardPoints;
+
+}
---
+++ src/data.js
@@ -0,0 +1,94 @@
+module.exports = {
+  James: [
+    {
+      date: "1/1/2020",
+      amount: 140
+    },
+    {
+      date: "2/1/2020",
+      amount: 100
+    },
+    {
+      date: "3/1/2020",
+      amount: 180
+    },
+    {
+      date: "1/1/2020",
+      amount: 450
+    },
+    {
+      date: "1/1/2020",
+      amount: 190
+    },
+    {
+      date: "3/1/2020",
+      amount: 200
+    },
+    {
+      date: "2/1/2020",
+      amount: 470
+    },
+    {
+      date: "3/1/2020",
+      amount: 220
+    }
+  ],
+  Gresin: [
+    {
+      date: "1/1/2020",
+      amount: 130
+    },
+    {
+      date: "2/28/2020",
+      amount: 190
+    },
+    {
+      date: "3/15/2020",
+      amount: 300
+    },
+    {
+      date: "2/11/2020",
+      amount: 450
+    },
+    {
+      date: "3/24/2020",
+      amount: 350
+    },
+    {
+      date: "2/2/2020",
+      amount: 100
+    }
+  ],
+  steveJobs: [
+    {
+      date: "2/1/2020",
+      amount: 354
+    },
+    {
+      date: "1/26/2020",
+      amount: 120
+    },
+    {
+      date: "3/4/2020",
+      amount: 114
+    },
+    {
+      date: "3/24/2020",
+      amount: 345
+    },
+    {
+      date: "1/2/2020",
+      amount: 230
+    },
+    {
+      date: "1/12/2020",
+      amount: 450
+    },
+    {
+      date: "2/12/2020",
+      amount: 50
+    }
+  ],
+  
+};
+
